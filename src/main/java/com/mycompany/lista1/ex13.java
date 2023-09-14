12397eab6cc3ac2f1c3fea02910d4dd04744fe3c
LEONARDO DOS SANTOS <leonardo.35903@aluno.pr.senac.br>
2023-09-14 16:23:26 -0300

    teste

--- src/main/java/com/mycompany/lista1/ex13.java
+++ src/main/java/com/mycompany/lista1/ex13.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mucompany.lista1;
+package com.mycompany.lista1;
 
 import java.util.Scanner;
 
@@ -10,37 +10,30 @@ import java.util.Scanner;
  *
  * @author leonardo.35903
  */
-public class ex14 {
+public class ex13 {
     public static void main(String args[]){
-        
+    
+        int cont = 0;
         String texto;
         Scanner sc = new Scanner(System.in);
         
         System.out.print("digite uma frase: ");
-        texto = sc.nextLine().replace(" ", "").toLowerCase();
+        texto = sc.nextLine();
         
         char[] chars = new char[texto.length()];
         
         chars = texto.toCharArray();
+    
+        for(int i = 0; i < chars.length; i++){
         
-        
-        for(int i = 0; i < chars.length / 2; i++){
-            
-            if(chars[i] == chars[chars.length - 1 - i]){
-            
-            continue;
-            
-            }else{
-            
-            System.out.print("A frase não é um palindromo");
-            System.exit(0);
-            
+            if(chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u'){
+                
+                cont++;
+                
             }
             
         }
         
-        
-        System.out.print("a frase é um palindromo");
-    
+        System.out.print("a frase digitada tem " + cont + " vogais.");
     }
 }
